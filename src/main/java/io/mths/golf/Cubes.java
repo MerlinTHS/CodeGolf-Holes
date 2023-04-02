536618a6afb32d592cb2e2aa86fb981a7f8595a2
Merlin Eden (THS) <m.eden@ths-bs.de>
2023-04-03 00:09:04 +0200

    [Cubes] Use one loop

--- src/main/java/io/mths/golf/Cubes.java
+++ src/main/java/io/mths/golf/Cubes.java
@@ -2,47 +2,51 @@ package io.mths.golf;
 
 public interface Cubes {
     static void main(String[] args) {
-        var result = "";
 
-        for (int cube = 1; cube < 8; ++cube) {
-            int cubePlusOne = cube + 1,
-                    width = 4 + 5 * cube,
+        for (int index = 1, cube = 1; cube < 8; ++index) {
+            // the cubePlusOne variable was replaced by ++cube -> cube --
+            int width = 4 + 5 * cube,
                     height = 3 + 3 * cube,
                     size = width * height,
                     vertex1 = 2 + cube,
                     vertex2 = width - 1,
-                    vertex3 = cubePlusOne * width + 1,
+                    vertex3 = ++cube * width + 1,
                     vertex4 = vertex1 * width - vertex1,
-                    vertex5 = size - cubePlusOne * width - 1,
-                    vertex6 = size - width + 1,
-                    vertex7 = size - vertex1;
+                    vertex5 = size - cube-- * width - 1,
+                    vertex6 = size - vertex2,
+                    vertex7 = size - vertex1,
+                    x = index % width,
+                    y = index / width;
 
-            for (int index = 1; index <= vertex7; ++index) {
-                int x = index % width,
-                        y = index / width;
 
-                result += index == vertex1
-                        | index == vertex2
-                        | index == vertex3
-                        | index == vertex4
-                        | index == vertex5
-                        | index == vertex6
-                        | index == vertex7
-                 ? "█" : x < 1
-                        ? "\n" : x == 1 & index > vertex3
-                        | x == vertex2 & index < vertex5
-                        | x == width - vertex1 & index > vertex4
-                        ? "│" : vertex1 + (y - vertex1 / width) * vertex2 == index
-                        | vertex2 + (y - vertex2 / width) * vertex2 == index & index < vertex4
-                        | vertex5 + (y - vertex5 / width) * vertex2 == index
-                        ? "╱" : index > vertex1 & index < vertex2
-                        | index > vertex3 & index < vertex4
-                        | index > vertex6
-                        ? "─" : " ";
-            }
-            result += "\n\n";
+            // If the index is at the last vertex, increment cube.
+            cube += index / vertex7;
+            System.out.print(
+                    index == vertex7 && (index = 0) < 1 // The second expression is used to reset index for the new cube.
+                            ? "█\n\n"
+                            : index == vertex1
+                                | index == vertex2
+                                | index == vertex3
+                                | index == vertex4
+                                | index == vertex5
+                                | index == vertex6
+                            ? "█"
+                            : x < 1
+                                ? "\n"
+                                : x == 1 & index > vertex3
+                                    | x == vertex2 & index < vertex5
+                                    | x == width - vertex1 & index > vertex4
+                                    ? "│"
+                                    : vertex1 + (y - vertex1 / width) * vertex2 == index
+                                        | vertex2 + (y - vertex2 / width) * vertex2 == index & index < vertex4
+                                        | vertex5 + (y - vertex5 / width) * vertex2 == index
+                                        ? "╱"
+                                        : index > vertex1 & index < vertex2
+                                            | index > vertex3 & index < vertex4
+                                            | index > vertex6
+                                            ? "─"
+                                            : " "
+            );
         }
-
-        System.out.print(result);
     }
 }
